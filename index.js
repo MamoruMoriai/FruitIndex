4f447c3bfd58c81bd7c4eadee28834f5824554c7
盛合克 <mamoru_moriai@moriaikatsu-no-MacBook-Air.local>
2018-05-15 15:10:25 +0900

    改正版

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var app = express();
 var data = ['apple', 'orange', 'mango', 'peach'];
 
 app.get('/apple', function(reg, res) {
-  res.send(data[0]);
+  res.send('Apple Index : ' + String(data.indexOf('apple')));
 });
 app.get('/orange', function(reg, res) {
-  res.send(data[1]);
+  res.send('Orange Index : ' + String(data.indexOf('orange')));
 });
 app.get('/mango', function(reg, res) {
-  res.send(data[2]);
+  res.send('Mango Index : ' + String(data.indexOf('mango')));
 });
 app.get('/peach', function(reg, res) {
-  res.send(data[3]);
+  res.send('Peach Index : ' + String(data.indexOf('peach')));
 });
 
 app.listen(process.env.PORT || 8080);
